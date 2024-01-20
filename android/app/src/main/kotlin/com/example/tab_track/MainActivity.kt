9e4f8229ee78d4b8dadd7d1f665950e253aaf021
shubhamkar <shubhamkar.nalkar@gmail.com>
2024-01-21 00:14:53 +0530

    initial

---
+++ android/app/src/main/kotlin/com/example/tab_track/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.tab_track
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/home/my_app.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class MyApp extends ConsumerStatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  ConsumerState<ConsumerStatefulWidget> createState() => _MyAppState();
+}
+
+class _MyAppState extends ConsumerState<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
